86efd532c014b44964989a3e7f27e8a0aa9f49e7
mitchellhooymans <80511083+mitchellhooymans@users.noreply.github.com>
2024-06-28 15:16:59 +1000

    created first file for project refactoring

---
+++ src/qarg_tools.py
@@ -0,0 +1,16 @@
+# This is the codebase for QARG tools, tools to be used by the QUT Astrophysics Research Group
+# This file is intended to be used and imported for use in the project. This
+# file contains functions, variables and code that will make the project easy 
+# to use in the future for this project
+
+# Import packages <-  note the dependencies
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from astLib import astSED
+from astropy.io import fits
+
+
+################################################################################################
+
