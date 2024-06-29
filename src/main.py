0f87463e3870232ca0d09c1c32d9432365bab47b
mitchellhooymans <80511083+mitchellhooymans@users.noreply.github.com>
2024-06-30 07:30:56 +1000

    added more to boilerplate

--- src/main.py
+++ src/main.py
@@ -10,18 +10,31 @@
 def main():
     """Main function to orchestrate the entire analysis pipeline."""
 
-    # 1. Parse command-line arguments (if any)
+    # Set this up to run the workflow on the first set of things
+    # essentially this should be a general program that generate outputs and 
+    # we don't have to worry about the specifics of what we are doing
+    
+    
+    # Design this so far with the intention of doing it with the inital theoeretical GALSEDATLAS + SWIRE Templates
+    
+    # 1. Choose Models
+    
+    # 1.1 SED Models
+    galaxy_templates = ['GALAXSEDATLAS', 'SWIRE']
+    agn_templates = 'SKIRTOR'
+    
+    # 1.2 Choice of Colour Space
+    colourspace = 'UVJ'
 
-    # 2. Set up logging
+    # 2. Load and preprocess data - Loads all data into the worksapce, models, astronomical colours.
+    
 
-    # 3. Load and preprocess data
+    # 3. Run Model Code - Creates composite models and the resulting astromical colours
 
-    # 4. Fit the model
- 
+    # 4. Visualize results - plot the colours on a graph
 
-    # 5. Visualize results
-
-    # 6. Save results (optional) - Output anything as required.
+    # 5. Save results (optional) - Output anything as required.
+   
     # ...
 
 
