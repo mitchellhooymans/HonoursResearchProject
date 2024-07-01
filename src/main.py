a29a0bef27dd6f2567a83fd0f9b8a139492403a1
mitchellhooymans <80511083+mitchellhooymans@users.noreply.github.com>
2024-07-01 16:49:46 +1000

    updated functionss and analysis pipeline

--- src/main.py
+++ src/main.py
@@ -1,33 +1,31 @@
 ## This is the script that will run the codebase. Ideally this script should be run,
 ## with certain parameters to get a particular output.
 
-# We will use the following libra
 
 
 
-
-
-def main():
+def run_analysis():
     """Main function to orchestrate the entire analysis pipeline."""
 
     # Set this up to run the workflow on the first set of things
     # essentially this should be a general program that generate outputs and 
     # we don't have to worry about the specifics of what we are doing
+    # 
     
     
     # Design this so far with the intention of doing it with the inital theoeretical GALSEDATLAS + SWIRE Templates
     
     # 1. Choose Models
     
-    # 1.1 SED Models
-    galaxy_templates = ['GALAXSEDATLAS', 'SWIRE']
+    # 1.1 SED Models - Place Holders for now
+    galaxy_templates = 'GALSEDATLAS'
     agn_templates = 'SKIRTOR'
     
-    # 1.2 Choice of Colour Space
+    # 1.2 Choice of Colour Space - This will determine the colour space we are investigating.
     colourspace = 'UVJ'
 
     # 2. Load and preprocess data - Loads all data into the worksapce, models, astronomical colours.
-    
+    data = load_preprocess_data(galaxy_templates, agn_templates, colourspace)
 
     # 3. Run Model Code - Creates composite models and the resulting astromical colours
 
--- src/qarg_tools.py
+++ src/qarg_tools.py
@@ -14,3 +14,54 @@ from astropy.io import fits
 
 ################################################################################################
 
+# Function to load and preprocess data
+
+def load_preprocess_data(galaxy_templates, agn_templates, colourspace):
+    """Function to load and preprocess data for the analysis pipeline."""
+    
+    # Load the galaxy templates
+    galaxy_templates = load_galaxy_templates(galaxy_template_set)
+    
+    # Load the AGN templates
+    agn_templates = load_agn_model(agn_model_name)
+    
+    # Load the astronomical colours
+    astronomical_colours = load_filters(colourspace)
+    
+    # Return the data
+    return galaxy_templates, agn_templates, astronomical_colours
+
+def load_galaxy_templates(galaxy_templates):
+    
+    
+    # Load the galaxy templates
+    galaxy_templates = galaxy_templates
+    
+    return galaxy_templates
+
+def load_agn_model(agn_templates):
+
+   # Load the AGN templates
+    agn_templates = agn_templates
+    
+    return agn_templates
+
+
+def load_filters(colourspace):
+    filter_1, filter_2, filter_3 = 0, 0, 0
+    # Switch statement to load the filters based on the colourspace
+    if colourspace == 'UVJ':
+        # Load the UVJ filters
+        
+        astronomical_colours = [a, b, c]
+    elif colourspace == 'NUVrK':
+        # Load the NUVrK filters
+        astronomical_colours = 'NUVrK'
+    elif colourspace == 'BzK':
+        # Load the BzK filters
+        astronomical_colours = 'BzK'
+    else:
+        # Load the default UVJ filters
+
+    
+    return astronomical_colours
