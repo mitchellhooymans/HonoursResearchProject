7e413edbc5873f87444a0a95d4d05ee11480b9c8
mitchellhooymans <80511083+mitchellhooymans@users.noreply.github.com>
2024-07-15 16:03:47 +1000

    added more functionality to the qt file

--- src/SED_Processing.py
+++ src/SED_Processing.py
@@ -20,10 +20,85 @@ import qarg_tools as qt
 # # Load Data
 # #filters = qt.load_passbands('UVJ')
 # #print(filters)
+
+
+# Load UVJ filters
 filters = qt.load_passbands('UVJ')
 print(filters[0])
 
 
+# load Type 1 model
+agn_model = qt.load_agn_model('Type 1')
+
+print(agn_model)
 
 
-# # Code logic
+# Create agn_model SED
+agn_sed = qt.create_sed(agn_model)
+
+print(agn_sed)
+
+# # Plot the AGN Model SED wl against flux
+# plt.figure(figsize=(10,6))
+# plt.loglog(agn_sed.wavelength, agn_sed.flux)  
+# plt.xlabel('Wavelength (Angstroms)')
+# plt.ylabel('Flux (erg/s/cm^2/A)')
+# plt.title('AGN Model SED')
+# plt.show()
+
+
+
+# Attempt to read in the SWIRE templates using the functions in qarg_tools.py
+swire_templates = qt.load_galaxy_template_set('SWIRE', 'datasets/templates/SWIRE')
+
+print(swire_templates['dataframe'][2])
+
+# Create an SED from this
+swire_sed = qt.create_sed(swire_templates['dataframe'][15])
+
+# # Plot the SED
+# plt.figure(figsize=(10,6))
+# plt.loglog(swire_sed.wavelength, swire_sed.flux)
+# plt.xlabel('Wavelength (Angstroms)')
+# plt.ylabel('Flux (erg/s/cm^2/A)')
+# plt.title('SWIRE SED')
+# plt.show()
+
+
+
+
+# Attempt to read in the SWIRE templates using the functions in qarg_tools.py
+brown_templates = qt.load_galaxy_template_set('BROWN', 'datasets/templates/BROWN/2014/rest')
+
+print(brown_templates['dataframe'][2])
+
+# Create an SED from this
+brown_sed = qt.create_sed(brown_templates['dataframe'][15])
+
+# # Plot the SED
+# plt.figure(figsize=(10,6))
+# plt.loglog(brown_sed.wavelength, brown_sed.flux)
+# plt.xlabel('Wavelength (Angstroms)')
+# plt.ylabel('Flux (erg/s/cm^2/A)')
+# plt.title('SWIRE SED')
+# plt.show()
+
+
+
+# combine the AGN and brown seds then plot
+combined_sed = qt.create_composite_sed(agn_sed, brown_sed, 0.5)
+
+
+# Plot the SED
+plt.figure(figsize=(10,6))
+plt.loglog(combined_sed.wavelength, combined_sed.flux, label='Combined SED')
+
+# plot the constituent SEDs
+plt.loglog(agn_sed.wavelength, agn_sed.flux, label='AGN SED')
+plt.loglog(brown_sed.wavelength, brown_sed.flux, label='Brown SED')
+plt.xlabel('Wavelength (Angstroms)')
+plt.ylabel('Flux (erg/s/cm^2/A)')
+plt.title('Combined SED')
+plt.show()
+
+
--- src/qarg_tools.py
+++ src/qarg_tools.py
@@ -29,6 +29,18 @@ from astropy.io import fits
 
 
 
+###
+
+# Importatant variables
+
+# possible parameters for the SKIRTOR models, any permutations of these parameters can be used to generate a model
+skirtor_params = {'tau': [3, 5, 7, 9, 11], 'p': [0, 0.5,1, 1.5], 'q': [0, 0.5,1, 1.5], 'oa': [10, 20, 30, 40, 50, 60, 70, 80], 'rr': [10, 20, 30], 'i': [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]}
+
+
+
+
+
+
 ######################################################################################################################
 
 
@@ -103,24 +115,9 @@ def load_passbands(colorspace, path='datasets\Filters'):
 
 ######################################################################################################################
 
-
-
-# TAU = [3, 5, 7, 9, 11] # Optical Depth - tau
-# P = [0, 0.5,1, 1.5] # radial gradient of dust density
-# Q = [0, 0.5,1, 1.5] # polar dust density gradient 
-# OA = [10, 20, 30, 40, 50, 60, 70, 80] # opening angle between equiltorial plane and edge of torus
-# RR = [10, 20, 30] # ratio of outer to inner radius
-# I = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] # inclination, the viewing angle of instrument w.r.t AGN
-
-# Dictionary to store the names of the models, using the values of the parameters
-SKIRTOR_PARAMS = {'tau': [3, 5, 7, 9, 11], 'p': [0, 0.5,1, 1.5], 'q': [0, 0.5,1, 1.5], 'oa': [10, 20, 30, 40, 50, 60, 70, 80], 'rr': [10, 20, 30], 'i': [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]}
-
-
-####################################################################################################
-
-
 # For interfacing with the SKIRTOR AGN models
 def read_skirtor_model(optical_depth, p, q, opening_angle, radius_ratio, inclination, folder_path='datasets/Templates/Skirtor'):
+    
     """_summary_
 
     Args:
@@ -136,10 +133,18 @@ def read_skirtor_model(optical_depth, p, q, opening_angle, radius_ratio, inclina
     """
     # Define the naming convention for reading in the Skirtor models
     filename = 't'+str(optical_depth)+'_p'+str(p)+'_q'+str(q)+'_oa'+str(opening_angle)+'_R'+str(radius_ratio)+'_Mcl0.97_i'+str(inclination)+'_sed.dat'
-    # Join the file to the path and then read in the file
-    filepath =os.path.join(folder_path, filename)
-    # Read in the file and convert it to a pandas dataframe
-    data = np.loadtxt(filepath, skiprows=5)
+        
+    
+    # use a Try and Except block to catch any file not found errors
+    try:
+        # Join the file to the path and then read in the file
+        filepath =os.path.join(folder_path, filename)
+        data = np.loadtxt(filepath, skiprows=5)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Model file not found: {filepath}")
+    
+    
+    
     
     # Convert it to a pandas dataframe # All fluxes are of the form lambda*F_lambda
     df = pd.DataFrame(data)
@@ -169,13 +174,10 @@ def read_skirtor_model(optical_depth, p, q, opening_angle, radius_ratio, inclina
 
 
 
-
-
-
 ######################################################################################################################
 
 # Load AGN Models code -this will only use SKIRTOR models for now - can be extended later
-def load_agn_model(agn_type, path='datasets/Templates'):
+def load_agn_model(agn_type, path='datasets/Templates/Skirtor'):
     """
     Loads an AGN model with the appropriate parameters.
 
@@ -187,6 +189,382 @@ def load_agn_model(agn_type, path='datasets/Templates'):
         pd.DataFrame: A dataframe containing the output of the chosen model.
     """
     
+    # TAU = [3, 5, 7, 9, 11] # Optical Depth - tau
+    # P = [0, 0.5,1, 1.5] # radial gradient of dust density
+    # Q = [0, 0.5,1, 1.5] # polar dust density gradient 
+    # OA = [10, 20, 30, 40, 50, 60, 70, 80] # opening angle between equiltorial plane and edge of torus
+    # RR = [10, 20, 30] # ratio of outer to inner radius
+    # I = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90] # inclination, the viewing angle of instrument w.r.t AGN
+    agn_type = agn_type.lower()
+    agn_types = ['type 1', 'type 2', 'intermediate']
+    # Check if agn type is supported
+    if agn_type not in agn_types:
+        raise ValueError(f"Unsupported AGN type: {agn_type}")    
+    else:
+
+        # Given the agn type, we can load the appropriate model
+        if agn_type == 'type 1':
+            # Load the type 1 model, using the paramaters 
+            tau = skirtor_params['tau'][2] # optical depth of 7
+            p = skirtor_params['p'][1] # Can make this either 0.5 or 0 (we choose 0.5)
+            q = skirtor_params['q'][0] # Make this zero, similar to the Ciesla paper
+            oa = skirtor_params['oa'][4] # opening angle of 50 degrees
+            rr = skirtor_params['rr'][1] # ratio of 20
+            i = skirtor_params['i'][0] # inclination of 0 degrees
+            
+        elif agn_type == 'type 2':
+            tau = skirtor_params['tau'][2] # optical depth of 7
+            p = skirtor_params['p'][1] # Can make this either 0.5 or 0 (we choose 0.5)
+            q = skirtor_params['q'][0] # Make this zero, similar to the Ciesla paper
+            oa = skirtor_params['oa'][4] # opening angle of 50 degrees
+            rr = skirtor_params['rr'][1] # ratio of 20
+            i = skirtor_params['i'][9] # inclination of 0 degrees
+            
+        elif agn_type == 'intermediate':
+            tau = skirtor_params['tau'][2] # optical depth of 7
+            p = skirtor_params['p'][1]  # Can make this either 0.5 or 0 (we choose 0.5)
+            q = skirtor_params['q'][0]  # Make this zero, similar to the Ciesla paper
+            oa = skirtor_params['oa'][4] # opening angle of 50 degrees
+            rr = skirtor_params['rr'][1] # ratio of 20
+            i = skirtor_params['i'][4] # inclination of 0 degrees
     
+        # print the paramsa
+        print(f"Loading AGN model: {agn_type} with parameters: tau={tau}, p={p}, q={q}, oa={oa}, rr={rr}, i={i}")
+    
+        # Load the model
+        model = read_skirtor_model(tau, p, q, oa, rr, i, path)
+        
+    return model
+
+
+######################################################################################################################
+
+# Create an astSED object of the AGN model
+def create_sed(model):
+    """
+    Create an astSED object from the model.
+
+    Args:
+        model (pd.DataFrame): The model dataframe.
+
+    Returns:
+        astSED.SED: The astSED object representing the model.
+    """
+    # Create the astSED object
+    sed = astSED.SED(model[['Wavelength']], model['Total Flux'])
+    return sed
+    
+######################################################################################################################
+
+def read_brown_galaxy_template(folder_path, name):
+    """
+        Reads in the galaxy template from the Brown 2014 dataset that has been restframed.
+
+    Args:
+        folder_path (string): path to the folder where the SED templates are located
+        name (string): name of the object
+    
+    Returns:
+        df: Returns a dataframe containing the SED template
+        objname: Returns the name of the object
+    """
+    folder_path = os.path.join(folder_path)
+    files_in_folder = os.listdir(folder_path)
+    
+    for file in files_in_folder:
+        # Find filepath and convert to df
+        objname = file.split('_restframe.dat')[0]
+        if objname == name:
+            filepath = os.path.join(folder_path, file)
+            data = np.loadtxt(filepath)
+            df = pd.DataFrame(data)
+            
+            # our wavelength is in microns, convert to Angstroms
+            df[0] = df[0] * 10000 # microns 10^-6 -> Angstroms 10^-10 
+        
+            # Name each of the columns appropriately
+            df.columns = ['lambda (Angstroms)', 'Luminosity (W/Hz)' , 'Total Flux (erg/s/cm^2/Angstrom)', 'Source']
+            
+            return df, objname
+        
+    
+    return None, None
+
+
+######################################################################################################################
+
+def read_brown_galaxy_templates(folder_path='datasets/Templates/Brown/2014/Rest'):
+    """_summary_
+
+        Args:
+            folder_path (string): path to the folder where the SED templates are located
+    
+        Returns:
+            df_list: Returns a list of dataframes containing the SED templates
+            objname_list: Returns a list of the names of the objects
+    """
+    df_list = []
+    objname_list = []
+    folder_path = os.path.join(folder_path)
+    files_in_folder = os.listdir(folder_path)
+    for file in files_in_folder:
+        
+        # Do a a try and except block to catch any file not found errors
+        try:
+            # Find filepath and convert to df
+            objname = file.split('_restframe.dat')[0]
+            filepath = os.path.join(folder_path, file)
+            data = np.loadtxt(filepath)
+            df = pd.DataFrame(data)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Model file not found: {filepath}")
+        
+        
+            
+        # our wavelength is in microns, convert to Angstroms
+        df[0] = df[0] * 10000 # microns 10^-6 -> Angstroms 10^-10 
+        
+        # Name each of the columns appropriately
+        #df.columns = ['lambda (Angstroms)', 'Luminosity (W/Hz)' , 'Total Flux (erg/s/cm^2/Angstrom)', 'Source']
+        df.columns = ['Wavelength', 'Luminosity' , 'Total Flux', 'Source']
+            
+        # Append the dataframe to the list    
+        df_list.append(df)
+        objname_list.append(objname)
+        
+        
+    return {'dataframe': df_list, 'name': objname_list}
+
+
+######################################################################################################################
+
+def read_swire_galaxy_templates(folder_path):
+    """_summary_
+
+    Args:
+        folder_path (string): folder path to the swire templates
+        name (string): name of template
+    
+    Returns:
+            df_list: Returns a list of dataframes containing the SED templates
+            objname_list: Returns a list of the names of the objects
+    """
+    df_list = []
+    objname_list = []
+    folder_path = os.path.join(folder_path)
+    files_in_folder = os.listdir(folder_path)
+    
+    # make sure to only read .sed files
+    file_extension = '.sed'
+    
+    # Filter files based on the specified file extension
+    files_in_folder = [file for file in files_in_folder if file.endswith(file_extension)]
+    
+    for file in files_in_folder:
+        
+        
+        # Do a a try and except block to catch any file not found errors
+        try:
+            # Find filepath and convert to df
+            objname = file.split('_template_norm.sed')[0]
+            filepath = os.path.join(folder_path, file)
+            data = np.loadtxt(filepath)
+            df = pd.DataFrame(data)
+        
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Model file not found: {filepath}")
+        
+        # Name each of the columns appropriately
+        # df.columns = ['Wavelength (Angstroms)', 'Total Flux (erg/s/cm^2/Angstrom)']
+        df.columns = ['Wavelength', 'Total Flux']
+            
+        # Append the dataframe to the list    
+        df_list.append(df)
+        objname_list.append(objname)
+    
+    return {'dataframe': df_list, 'name': objname_list}
+
+
+######################################################################################################################
+
+# read SED templates: Should read in whichever set of templates are in the directory and specified
+def load_galaxy_template_set(template_set, path):
+    """
+    Loads the entire galaxy template set.
+
+    Args:
+        template_set (str): The template set we are wanting to load
+
+    Returns:
+        pd.DataFrame: A collection of templates from the chosen set 
+    """
+    
+    # format string correctly
+    template_set = template_set.upper()
+    
+    # Check if template set is supported
+    template_sets = ['SWIRE', 'BROWN']
+    
+    if template_set not in template_sets:
+        raise ValueError(f"Unsupported template set: {template_set}")
+    else:
+        if template_set == 'SWIRE':
+            # Read in swire templates
+            templates = read_swire_galaxy_templates(path)
+        elif template_set == 'BROWN':
+            # Read in brown templates
+            templates = read_brown_galaxy_templates(path)
+           
+    return templates
+
+
+######################################################################################################################
+
+def calc_flux_integral(sed):
+    """
+    Calculate the integral of the flux with respect to the wavelength.
+
+    Args:
+        sed (SED object): Takes an SED with a flux and wavelength and computes a resulting integral.
+
+    Returns:
+        _type_: Returns the result of an integral over the total flux with respect to the wavelength
+    """
+    return np.trapz(sed.flux, sed.wavelength)
+
+
+######################################################################################################################
+
+def compute_scaling_factor(agn_sed, galaxy_sed):
+    """
+    Calculates the scaling factor required to match the flux of the AGN and galaxy SEDs.
+
+    Args:
+        agn (SED object): the first SED of the AGN
+        galaxy (SED object): the second SED of the galaxy
+
+    Returns:
+        float: scaling factor to match the flux of the AGN and galaxy SEDs
+    """
+    # Integrate the flux to get the total flux
+    integrated_agn_flux = calc_flux_integral(agn_sed)
+    integrated_galaxy_flux = calc_flux_integral(galaxy_sed)
+
+    scaling_factor = integrated_galaxy_flux/integrated_agn_flux
+    
+    return scaling_factor
+
+######################################################################################################################
+  
+def adjust_wavelength_range(sed_1, sed_2):
+    """
+    Given a particular SED with a particular wavelength range, and another SED with a different but similar wavelength
+    range, make a combined wavelength range for both of these values, cutting off both SEDS at the minimum SED value of the two
+    and return two SEDs that are comparable.
+
+    Args:
+    sed_1 (dataframe): The first sed dataframe
+    sed_2 (dataframe): the second sed dataframe
+    """
+    # Given an SED
+    wavelengths_sed1 = sed_1.wavelength
+    flux_sed1 = sed_1.flux
+
+    #print(sed_2['lambda (Angstroms)'])
+    wavelengths_sed2 = sed_2.wavelength
+    flux_sed2 = sed_2.flux
+
+    # Get a shared wavelength range across both SEDS
+    combined_wavelengths = np.union1d(wavelengths_sed1, wavelengths_sed2)
+
+    # Interpolate flux values for the combined wavelengths
+    combined_flux_sed1 = np.interp(combined_wavelengths, wavelengths_sed1, flux_sed1, left=np.nan, right=np.nan)
+    combined_flux_sed2 = np.interp(combined_wavelengths, wavelengths_sed2, flux_sed2, left=np.nan, right=np.nan) 
+
+    # We would like to see which sed has the min wavelength , and max wavelength,
+    # Cut the AGN and Galaxy model so they are within range of the original swire model
+    min_wavelength = np.max([np.min(wavelengths_sed1), np.min(wavelengths_sed2)])
+    max_wavelength = np.min([np.max(wavelengths_sed1), np.max(wavelengths_sed2)])
+
+    # Cut the AGN model
+    mask = (combined_wavelengths >= min_wavelength) & (combined_wavelengths <= max_wavelength)
+    combined_wavelengths = combined_wavelengths[mask]
+    combined_flux_sed1 = combined_flux_sed1[mask]
+    combined_flux_sed2 = combined_flux_sed2[mask]
+
+    # Create new sedobjects
+    new_sed1 = astSED.SED(combined_wavelengths, combined_flux_sed1)
+    new_sed2 = astSED.SED(combined_wavelengths, combined_flux_sed2)
+    
+
+    return new_sed1, new_sed2
+  
+######################################################################################################################  
+     
+# Function to create a single SED composite from 1 galaxy SED and 1 AGN model
+def create_composite_sed(agn_sed, gal_sed, alpha, beta=1):
+    """ Creates a composite galaxy/agn sed with a given weight of the AGN SED and Galaxy SED
+    mixing can be done if beta = 1 - alpha.
+    Args:
+        agn_df (astSED.SED object): agn dataframe
+        gal_sed (astSED.SED): galaxy dataframe
+        alpha (int): value from 0 to 1 the weight of the AGN SED in the composite SED
+        beta (int): value from 0 to 1, the weight of the galaxy SED in the composite SED (default to 1 to allow for addition)
+
+    Returns:
+        astSED.SED Object: composite SED object
+    """
+    
+    # Ensure that the SED is of the same wavelength range, interpolating as required
+    agn_sed, gal_sed = adjust_wavelength_range(agn_sed, gal_sed)
+    
+    
+    # Remove them
+    #gal_sed = gal_sed.dropna()
+    
+    # Normalize the flux of the AGN and Galaxy SEDs
+    scaling_factor = compute_scaling_factor(agn_sed, gal_sed)
+    
+    # Scaled flux densities
+    agn_sed.flux = agn_sed.flux * scaling_factor
+    
+    # Sum the flux values at each wavelength
+    combined_flux = alpha * agn_sed.flux + beta * gal_sed.flux
+    
+    # use the wavelength of the galaxy SED or AGN sed
+    combined_wavelengths = gal_sed.wavelength
+    
+    # create a new astSED object
+    composite_sed = astSED.SED(combined_wavelengths, combined_flux)
+    
+    return composite_sed
+
+######################################################################################################################
+
+# Creating a set of SEDS for a given AGN model and a set of galaxy templates
+# function to create a set of composites
+def generate_composite_set(agn_sed, gal_sed_list, alpha_list):
+    """
+    Generate a set of composite SEDs for a given AGN model and a set of galaxy templates.
+
+    Args:
+        agn_sed (astSED.SED object): SED object that contains the SED of the AGN model
+        gal_sed_list (list): list of SED objects that contains the SED of the galaxy templates
+        alpha_list (list): list of alpha values, that each composite will be made with
+
+    Returns:
+        _type_: a list of lists of astSED.SED objects that contains the composite SEDs
+    """
+    # Create a list of composite SEDs
+    composite_alpha_list = []
+    # Loop through the list of alpha values
+    for alpha in alpha_list:
+        composite_sed_list = []
+        for gal_sed in gal_sed_list:
+            agn_model = agn_sed # ensures that each composite is made with the inital AGN model and it isn't adjusted.
+            composite_sed = create_composite_sed(agn_model, gal_sed, alpha)
+            composite_sed_list.append(composite_sed) # list of composites at a particular alpha value
+        composite_alpha_list.append(composite_sed_list)
+    return composite_alpha_list # contains a list containing a the list of composites for each alpha value
 
     
