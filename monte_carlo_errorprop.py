6c43d7a4311ee4269d659f3793b33151bc7e542f
mitchellhooymans <80511083+mitchellhooymans@users.noreply.github.com>
2024-07-24 13:00:02 +1000

    monte carlo script outline

---
+++ monte_carlo_errorprop.py
@@ -0,0 +1,4 @@
+# Monte Carlo Simulation for Error Proagation 
+# This script will be intended to be used for the error propagation of the
+# the uncertainities for the SEDs, as no uncertainities are provided by CIGALE the best way to model this
+# will be by getting the error propogation from the Monte Carlo simulation modelling the errors as a gaussian distribution
